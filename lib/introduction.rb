e899c0f18e0f15f67a36725de2f43f0dcfeaf93a
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-03 02:41:46 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,6 @@ def introduction(name)
   
 end
 
-def introduction_with_language(name, language)
-  puts `"Hi,my name is #{name} and I am learning to program in #{language}
+def introduction_with_language(name,language)
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
